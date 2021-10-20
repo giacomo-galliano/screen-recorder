24a45291352d5021d1a5b8e34ea6265ac126bc5d
giacomo <giacomo.galliano97.gg@gmail.com>
2021-10-20 22:13:27 +0200

    removed deprecated commands

---
+++ build/CMakeFiles/screen-recorder.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/giacomo/Documents/Poli/I.II_Programmazione di sistema/screen-recorder/src/main.cpp" "/home/giacomo/Documents/Poli/I.II_Programmazione di sistema/screen-recorder/build/CMakeFiles/screen-recorder.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
+extern "C"
+{
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libavdevice/avdevice.h>
+}
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
     AVFormatContext *pFormatCtx; //it contains the informations about the format
 
     pFormatCtx = avformat_alloc_context(); //allocate the memory to the AVFormatContext component
-    if (!pFormatCtx) {
+    if (!pFormatCtx)
+    {
         printf("Couldn't create AVFormatContext\n");
         return -1;
     }
@@ -16,21 +21,23 @@ int main(int argc, char **argv){
     /*
     *   Linux - we'll use "x11grub"
     */
-   AVInputFormat *ift = av_find_input_format("x11grab");
-   /*
+    AVInputFormat *ift = av_find_input_format("x11grab");
+    /*
    * avformat_open_input(AVFormatContext, filename, AVInputFormat, AVDictionary)
    * this function is used to open the file and read its header
    * 
    * The last three arguments are used to specify the file format, 
    * buffer size, and format options, but by setting this to NULL or 0, libavformat will auto-detect these. 
    */
-   if(avformat_open_input(&pFormatCtx, ":0.0", ift, NULL)!=0){
-       printf("Couldn't open the video file\n");
-       return -1;
-   };
+    if (avformat_open_input(&pFormatCtx, ":0.0", ift, NULL) != 0)
+    {
+        printf("Couldn't open the video file\n");
+        return -1;
+    };
 
     // Now we retrieve the stream informations. It populates pFormatCtx->streams with the proper infos
-    if(avformat_find_stream_info(pFormatCtx, NULL)<0){
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+    {
         //the function only looks at the header, so we must check out the stream informations
         printf("Couldn't find stream informations\n");
         return -1;
@@ -39,27 +46,33 @@ int main(int argc, char **argv){
     // Now pFormatCtx->streams is just an array of pointers, of size pFormatCtx->nb_streams
 
     // Walk through the streams until we find a video stream
-    int videoIndex=-1;
-	for(int i=0; i<pFormatCtx->nb_streams; i++) 
-		if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO)
-		{
-			videoIndex=i;
-			break;
-		}
-	if(videoIndex==-1)
-	{
-		printf("Didn't find a video stream.\n");
-		return -1;
-	}
+    int videoIndex = -1;
+    for (int i = 0; i < pFormatCtx->nb_streams; i++)
+        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            videoIndex = i;
+            break;
+        }
+    if (videoIndex == -1)
+    {
+        printf("Didn't find a video stream.\n");
+        return -1;
+    }
 
     AVCodecContext *pCodecCtx = NULL; //it will contain the stream's information about the codec
     // Get a pointer to the codec context for the video stream
-    pCodecCtx = pFormatCtx->streams[videoIndex]->codec;
+    int result = avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[videoIndex]->codecpar);
+    if (result < 0)    {
+        //failed to set parameters
+        avformat_close_input(&pFormatCtx);
+        avcodec_free_context(&pCodecCtx);
+    }
 
     //Now we need to find the actual codec and open it
     AVCodec *pCodec = NULL;
     pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-    if(pCodec == NULL){
+    if (pCodec == NULL)
+    {
         printf("Codec not supported.\n");
         return -1;
     }
@@ -77,17 +90,14 @@ int main(int argc, char **argv){
     if(avcodec_copy_context())
     */
 
-   // open codec. options -> NULL
-    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0){
+    // open codec. options -> NULL
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+    {
         printf("Could not open the codec.\n");
         return -1;
     }
 
     /* STORING THE DATA */
-    AVFrame *pFrame = NULL; // the place to actually store the frame
+    AVFrame *pFrame = NULL;    // the place to actually store the frame
     pFrame = av_frame_alloc(); // allocate video frame
-
-    
-
-
 }
